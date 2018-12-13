ebc37396ebd3dfae0cc0731e6942cfcb4441c015
boazcogan <boazcogan@yahoo.com>
2018-12-13 13:29:46 -0800

    added listop

--- P3T6.py
+++ P3T6.py
@@ -4,15 +4,15 @@ from objectFunctionality import *
 
 def even_copy(ls): 
 	
-	if 	ls:
-		if 		ls		
+	if 	listp(	ls	):
+		if nullp(ls)
 			" "
 
-		if 		cdr(		ls) 		
+		if nullp(listop(cdr,ls) )
 			" "
 
 		else:
-			cons(			car(			cdr(			ls) ) 			even_copy			cddr(			ls) )
+			cons(			listop(car,listop(cdr,ls) ) 			even_copy			listop(cddr,ls) )
 
 ':
 		"even_copy_requires_a_list_argument"
@@ -21,12 +21,12 @@ def even_copy(ls):
 
 def list_sum(ls): 
 	
-	if 	ls:
-		if 		ls:
+	if 	listp(	ls	):
+		if 		nullp(		ls		):
 			0(:
-			if 			car(			ls) :
-				car(				ls) +list_sumcdr(ls) (:
-				0+list_sumcdr(ls) 
+			if 			numberp(			listop(car,ls) 			):
+				listop(car,ls) +list_sumlistop(cdr,ls) (:
+				0+list_sumlistop(cdr,ls) 
 
 
 
